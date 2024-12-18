222d013c722e9932b611dcc8606419ae7aae3fb3
David Olowomeye <olowomeyedavid242@gmail.com>
2024-12-18 23:25:37 +0000

    Added the endpoint to delete users

--- routes/user.go
+++ routes/user.go
@@ -107,3 +107,23 @@ func UpdateUser(c *fiber.Ctx) error {
 
 	return c.Status(200).JSON(responseUser)
 }
+
+func DeleteUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id id an integer")
+	}
+
+	if err := findUser(id, &user); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := database.Database.Db.Delete(&user).Error; err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	return c.Status(200).SendString("Successfully deleted user")
+}
