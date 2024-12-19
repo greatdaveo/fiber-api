2aea62c9d2d2495f40b0ee43e2c95e9db2f9fc8b
David Olowomeye <olowomeyedavid242@gmail.com>
2024-12-19 09:28:57 +0000

    Added the endpoint to create product

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/users/:id", routes.GetUser)
 	app.Put("/api/users/:id", routes.UpdateUser)
 	app.Delete("/api/users/:id", routes.DeleteUser)
+	// Product endpoint
+	app.Post("/api/products", routes.CreateProduct)
 
 }
 
---
+++ routes/products.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/greatdaveo/fiber-api/database"
+	"github.com/greatdaveo/fiber-api/models"
+)
+
+type ProductSerializer struct {
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	SerialNumber string `json:"serial_number"`
+}
+
+func CreateResponseProduct(productModel models.Product) ProductSerializer {
+	return ProductSerializer{ID: productModel.ID, Name: productModel.Name, SerialNumber: productModel.SerialNumber}
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	var product models.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	database.Database.Db.Create(&product)
+
+	responseProduct := CreateResponseProduct(product)
+
+	return c.Status(201).JSON(responseProduct)
+}
+
